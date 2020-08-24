86ab00da681ddff8f95a4985bc0badca480aceeb
Amir <amirdiafi2@gmail.com>
2020-08-24 14:45:03 +0100

    add files first time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Home from './components/Home'
+import {BsCodeSlash} from 'react-icons/bs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() { 
+    return ( 
+      <React.Fragment>
+        <div className="overlay"></div>
+        <Home />
+        <div className='footer-line'>
+          <BsCodeSlash /> ByAmir
+          <span></span>
+        </div>
+      </React.Fragment>
+     )
+  }
 }
-
+ 
 export default App;
